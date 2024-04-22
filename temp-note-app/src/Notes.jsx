c95c5f79d797786b07fa424cc1d476064b5eff42
Roshan KC <roshankc.py@gmail.com>
2024-04-22 23:50:46 +0545

    new_file_added:

---
+++ temp-note-app/src/Notes.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Notes = () => {
+  return (
+    <>
+      <div className="note">
+        <h1> Title</h1>
+        <br />
+        <p>I am the note</p>
+        <span>❎ </span>
+      </div>
+    </>
+  );
+};
+
+export default Notes;
